77ae4e887af7e9643544b8cc00047615ea7de57d
Amine Mustapha Rachid <AmineRA2006@gmail.com>
2025-08-19 08:07:40 +0100

    Fixed font size too big in no fee section

--- src/components/nofee.tsx
+++ src/components/nofee.tsx
@@ -4,8 +4,8 @@ export default function NoFee() {
     return (
         <div className="bg-white flex flex-col items-center gap-10 lg:max-w-2/3 mx-auto p-4 md:p-10">
             <div className="flex flex-col items-center">
-                <h1 className={`font-semibold text-7xl text-center text-[rgb(29,29,31)]`}>What&apos;s better than music?</h1>
-                <h1 className={`font-semibold text-7xl text-center text-[rgb(29,29,31)] pb-10`}>Music that&apos;s free.</h1>
+                <h1 className={`font-semibold text-6xl md:text-7xl text-center text-[rgb(29,29,31)]`}>What&apos;s better than music?</h1>
+                <h1 className={`font-semibold text-6xl md:text-7xl text-center text-[rgb(29,29,31)] pb-10`}>Music that&apos;s free.</h1>
                 <p className={`font-semibold text-xl text-[#6E6E73] text-center lg:max-w-4/5`}>With iTuneUp, there are no barriers between you and your music. No subscriptions. No limits. Freedom built in. Just everything you love, ready to download and enjoy.</p>
             </div>
             <div className="flex flex-row items-center gap-10">
